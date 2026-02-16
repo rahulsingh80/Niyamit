4bcf44327a6bfe07185b265ce61709a94a31ddef
Rahul Singh <rsingh@thomas-daily.de>
2026-02-16 12:43:49 +0100

    Upload dist folder to setup Github Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Niyamit/",
   resolve: {
     alias: {
       "@domain": path.resolve(__dirname, "src/domain"),
